4ae7ebffab2002d50d4c83fd8baea834bd47b639
Mari Kalyuzhnaya <marinakaluzhnaya13@gmail.com>
2021-09-13 13:43:30 +0300

    fix: 💡 solves next + storybook configuration issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,13 @@
+const path = require("path");
+
 const BASE_URL = process.env.STORYBOOK_BASE_URL;
 const baseTag = BASE_URL ? `<base href="${BASE_URL}">` : '';
+const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../pages/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-knobs",
@@ -12,4 +15,16 @@ module.exports = {
     "@storybook/addon-essentials"
   ],
   managerHead: head => [head, baseTag].filter(Boolean).join("\n"),
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../'),
+      "@emotion/core": toPath("node_modules/@emotion/react"),
+      "@emotion/styled": toPath("node_modules/@emotion/styled"),
+      "emotion-theming": toPath("node_modules/@emotion/react"),
+    };
+
+    return config;
+  },
 }
