7639a5fda27c8ac2791339d4b58f60d3efe47e1d
Mari Kalyuzhnaya <marinakaluzhnaya13@gmail.com>
2021-09-23 13:39:49 +0300

    fix: 💡 fixes next, babel & typescript configs to resolve next issues

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      }
-    }
-
-    return config
-  },
 }
